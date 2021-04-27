bd9392661d4071dc2bd438f2dcd798772fa2b5dd
Cody Kimb <codykimbell@gmail.com>
2021-04-27 14:48:05 -0500

    optimize imgs

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,38 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+      },
+      output: {
+        filename: "[name].bundle.js",
+        path: __dirname + "/dist"
+      },
+      module: {
+        rules: [
+          {
+            test: /\.jpg$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name(file) {
+                    return '[path][name].[ext]';
+                  },
+                  publicPath: function(url) {
+                    return url.replace('../', '/assets/');
+                  }
+                }
+              },
+              {
+                loader: 'image-webpack-loader'
+              }
+            ]
+          }
+        ]
       },
       plugins: [
         new webpack.ProvidePlugin({
@@ -18,6 +46,5 @@ module.exports = {
           analyzerMode: "static", // the report outputs to an HTML file in the dist folder
         })
       ],    
-      
     mode: 'development'
 };
